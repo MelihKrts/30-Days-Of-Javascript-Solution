477722e9e937a0a0efd5dac1bb1ae77728df4c2f
MelihKrts <melih.karatas1281@gmail.com>
2023-07-05 13:27:02 +0300

    feat:[added Day 6 Solution]

---
+++ Day_6_Solution/Exercise_1/script.js
@@ -0,0 +1,186 @@
+// Day 6 Exercise Solution
+
+// Exercise Level 1
+
+// Exercise 1.1
+
+// For loop
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// While loop
+
+let i = 0;
+while (i < 10) {
+    i++;
+    console.log(i)
+}
+
+// Do-while loop
+
+let i = 0;
+do {
+    i++;
+    console.log(i);
+}
+while (i < 10)
+
+// Exercise 1.2
+
+// For loop
+
+for (let i = 10; i > 0; i--) {
+    console.log(i)
+}
+
+// While loop
+
+let i = 10;
+while (i > 0) {
+    i--;
+    console.log(i)
+}
+
+// Do-while loop
+
+let i = 10;
+do {
+    i--;
+    console.log(i);
+} while (i > 0)
+
+// Exercise 1.3
+
+let n = 100; // Custom number
+
+for (let i = 0; i < n; i++) {
+    console.log(i);
+}
+
+
+// Exercise 1.4
+
+let string = "";
+for (let i = 0; i < 7; i++) {
+    string += "#";
+    console.log(string);
+}
+
+// Exercise 1.5
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`${i} * ${i} = ${i * i}`);
+}
+
+// Exercise 1.6
+
+let string = "i  i^2  i^3";
+console.log(string);
+for (let i = 0; i <= 10; i++) {
+    console.log(` ${i}  ${i ** 2}  ${i ** 3}`);
+}
+
+// Exercise 1.7
+
+for (let i = 0; i < 100; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+// Exercise 1.8
+
+for (let i = 0; i < 100; i++) {
+    if (i % 2 == 1) {
+        console.log(i);
+    }
+}
+
+// Exercise 1.9
+
+/*
+
+https://onecompiler.com/javascript/3ww4c5kmq
+
+*/
+
+let count = 0
+let i, j
+for (j = 2; j <= 100; j++) {
+    for (i = 1; i <= j; i++) {
+        if (j % i == 0)
+            count++
+
+    }
+
+    if (count == 2)
+        console.log(j)
+    count = 0
+}
+
+// Exercise 1.10
+
+let sum = 0;
+for (let i = 0; i <= 100; i++) {
+    sum += i;
+}
+console.log(`The sum of all numbers from 0 to 100 is ${sum}.`);
+
+// Exercise 1.11
+
+let evens = 0;
+let odd = 0;
+
+for (let a = 0; a <= 100; a++) {
+    if (a % 2 == 0) {
+        evens += a;
+    }
+    if (a % 2 == 1) {
+        odd += a;
+    }
+}
+console.log(`The sum of all evens from 0 to 100 is ${evens}. And the sum of all odds from 0 to 100 is ${odd}`)
+
+// Exercise 1.12
+
+let evens = 0;
+let odd = 0;
+
+for (let a = 0; a <= 100; a++) {
+    if (a % 2 == 0) {
+        evens += a;
+    }
+    if (a % 2 == 1) {
+        odd += a;
+    }
+}
+
+
+const sumArray = [];
+let split = sumArray.push(evens,odd);
+
+console.log(sumArray)
+
+// Exercise 1.13
+
+let randomNumber = [];
+for (let i = 0; i < 5; i++) {
+    randomNumber.push(Math.floor(Math.random() * 10));
+}
+console.log(randomNumber);
+
+// Exercise 1.14
+
+var random = [];
+while (random.length < 5) {
+    var add = Math.floor(Math.random() * 10) + 1;
+    if (random.indexOf(add) === -1) random.push(add);
+
+}
+console.log(random);
+
+// Exercise 1.15
+const result = Math.random().toString(36).substring(2,7);
+console.log(result);
---
+++ Day_6_Solution/Exercise_2/script.js
@@ -0,0 +1,233 @@
+//  Day 6 Exercise Solution
+
+// Exercise Level 2
+
+// Exercise 2.1
+
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+}
+
+console.log(makeid(30));
+
+// Exercise 2.2
+
+const hexCode = () => {
+    let hex = (Math.random() * 0xfffff * 100000).toString(16);
+    return '#' + hex.slice(0, 6);
+};
+
+console.log(hexCode());
+
+// Exercise 2.3
+
+function randomRgb() {
+    var x = Math.floor(Math.random() * 255);
+    var y = Math.floor(Math.random() * 255);
+    var z = Math.floor(Math.random() * 255);
+    var rgbColor = `rgb(${x},${y},${z})`;
+    return rgbColor
+}
+
+console.log(randomRgb());
+
+// Exercise 2.4
+
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+];
+
+for (let i = 0; i < countries.length; i++) {
+    countries[i] = countries[i].toUpperCase();
+}
+
+console.log(countries);
+
+// Exercise 2.5
+
+for (let i = 0; i < countries.length; i++) {
+    countries[i] = countries[i].length;
+}
+console.log(countries);
+
+// Exercise 2.6
+
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+];
+
+let short = '';
+for (let i = 0; i < countries.length; i++) {
+    short += countries[i].slice(0, 3 ,'').toUpperCase();
+}
+console.log(short);
+
+
+const emptyArray = [];
+const countryCode = ['ALB', 'BOL', 'CAN', 'DEN', 'ETH', 'FIN', 'GER', 'HUN', 'IRE', 'ICE', 'JAP', 'KEN'];
+for (let i = 0; i < countries.length; i++) {
+    emptyArray.push([countries[i], countryCode[i], countries.length])
+}
+console.log(emptyArray);
+
+// Exercise 2.7
+
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+];
+
+const arr = [];
+for (let i = 0; i < countries.length; i++) {
+    if (countries[i].includes('land'))
+        arr.push(countries[i])
+}
+if (arr.length > 0)
+    console.log(arr);
+else
+    console.log('All these are countries without land')
+
+// Exercise 2.8
+
+const arr = [];
+for (let i = 0; i < countries.length; i++) {
+    if (countries[i].includes('ia'))
+        arr.push(countries[i])
+}
+if (arr.length > 0)
+    console.log(arr);
+else
+    console.log('All these are countries without land')
+
+// Exercise 2.9
+
+/* 
+https://stackoverflow.com/questions/6521245/finding-longest-string-in-array
+*/
+
+let longest = countries.sort((a, b) => b.length - a.length)[0];
+console.log(longest);
+
+Exercise 2.10
+
+let fiveArr = [];
+for (let i = 0; i < countries.length; i++) {
+    if (countries[i].length === 5) {
+        fiveArr.push(countries[i]);
+    }
+}
+console.log(fiveArr);
+
+// Exercise 2.11
+
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+]
+
+let longest = webTechs.sort((a, b) => b.length - a.length)[0];
+console.log(longest);
+
+// Exercise 2.12
+
+let allWebTechs = [];
+for (let i = 0; i < webTechs.length; i++) {
+    allWebTechs.push([webTechs[i], webTechs[i].length])
+}
+console.log(allWebTechs)
+
+// Exercise 2.13
+
+const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+let mern = '';
+for (let i = 0; i < mernStack.length; i++) {
+    mern += mernStack[i].slice(0, 1);
+}
+console.log(mern);
+
+// Exercise 2.14
+
+const array = ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"];
+
+for (let techs of array) {
+    console.log(techs);
+}
+
+// Exercise 2.15
+
+/* 
+
+https://codingbeautydev.com/blog/javascript-reverse-array-without-modifying/
+
+*/
+
+const fruit = ['banana', 'orange', 'mango', 'lemon'];
+const reverse = [];
+for (let i = fruit.length - 1; i >= 0; i--) {
+    reverse.push(fruit[i]);
+}
+console.log(reverse);
+
+// Exercise 2.16
+
+const fullStack = [
+    ['HTML', 'CSS', 'JS', 'React'],
+    ['Node', 'Express', 'MongoDB']
+]
+
+fullStack.forEach((fullStack) => {
+    fullStack.forEach((techs) => {
+        var upper = techs.toUpperCase();
+        console.log(upper);
+    })
+})
+
+/* 
+
+https://www.programiz.com/javascript/multidimensional-array
+
+*/
+
+
