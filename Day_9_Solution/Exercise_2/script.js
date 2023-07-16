a21e2574859dd63dc0dd27f58fc194cf5098dbfe
MelihKrts <melih.karatas1281@gmail.com>
2023-07-16 14:10:47 +0300

    code:[edited script.js]

--- Day_9_Solution/Exercise_2/script.js
+++ Day_9_Solution/Exercise_2/script.js
@@ -3,7 +3,7 @@
 // Day 9 Exercise Level 2 Solution
 
 
-// const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
 const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const products = [
@@ -17,84 +17,84 @@ const products = [
 
 // Exercise 2.1
 
-// const totalPrice = products.map(product => product.price).filter(price => price > 0).reduce((accumulator, currentPrice) => accumulator + currentPrice, 0);
-// console.log(totalPrice);
+const totalPrice = products.map(product => product.price).filter(price => price > 0).reduce((accumulator, currentPrice) => accumulator + currentPrice, 0);
+console.log(totalPrice);
 
 // Exercise 2.2
 
-// const totalPrice = products.reduce((accumulator, currentProduct) => {
-//     const price = parseFloat(currentProduct.price);
-//     if (!isNaN(price)) {
-//         return accumulator + price;
-//     }
-//     return accumulator
-// }, 0);
+const totalPrice = products.reduce((accumulator, currentProduct) => {
+    const price = parseFloat(currentProduct.price);
+    if (!isNaN(price)) {
+        return accumulator + price;
+    }
+    return accumulator
+}, 0);
 
-// console.log(totalPrice);
+console.log(totalPrice);
 
 // Exercise 2.3
 
 const countries = require('./countries_data.js');
 
-// function categorizeCountries() {
-//   const patterns = ['land', 'ia', 'island', 'stan'];
-//   const categorizedCountries = {};
+function categorizeCountries() {
+  const patterns = ['land', 'ia', 'island', 'stan'];
+  const categorizedCountries = {};
 
-//   patterns.forEach((pattern) => {
-//     const countriesWithPattern = countries.filter((country) =>
-//       country.name.toLowerCase().includes(pattern.toLowerCase())
-//     );
-//     categorizedCountries[pattern] = countriesWithPattern;
-//   });
+  patterns.forEach((pattern) => {
+    const countriesWithPattern = countries.filter((country) =>
+      country.name.toLowerCase().includes(pattern.toLowerCase())
+    );
+    categorizedCountries[pattern] = countriesWithPattern;
+  });
 
-//   return categorizedCountries;
-// }
+  return categorizedCountries;
+}
 
-// const categorizedCountries = categorizeCountries();
-// console.log(categorizedCountries);
+const categorizedCountries = categorizeCountries();
+console.log(categorizedCountries);
 
 
 // Exercise 2.4
 
-// function countStartingLetters() {
-//   const letterCounts = {};
-//   countries.forEach((country) => {
-//     const firstLetter = country.name.charAt(0).toUpperCase();
-//     if (/[A-Z]/.test(firstLetter)) {
-//       letterCounts[firstLetter] = (letterCounts[firstLetter] || 0) + 1;
-//     }
-//   });
+function countStartingLetters() {
+  const letterCounts = {};
+  countries.forEach((country) => {
+    const firstLetter = country.name.charAt(0).toUpperCase();
+    if (/[A-Z]/.test(firstLetter)) {
+      letterCounts[firstLetter] = (letterCounts[firstLetter] || 0) + 1;
+    }
+  });
 
-//   const resultArray = Object.entries(letterCounts).map(([letter, count]) => ({
-//     letter,
-//     count,
-//   }));
+  const resultArray = Object.entries(letterCounts).map(([letter, count]) => ({
+    letter,
+    count,
+  }));
 
-//   return resultArray;
+  return resultArray;
 
-// }
+}
 
-// console.log(countStartingLetters());
+console.log(countStartingLetters());
 
 
 // Exercise 2.5
 
-// function getFirstTenCountries() {
-//   const firstTenCountries = countries.slice(0, 10);
-//   return firstTenCountries;
-// }
+function getFirstTenCountries() {
+  const firstTenCountries = countries.slice(0, 10);
+  return firstTenCountries;
+}
 
-// console.log(getFirstTenCountries())
+console.log(getFirstTenCountries())
 
 // Exercise 2.6
 
-// function getLastTenCountries() {
+function getLastTenCountries() {
 
-//   const lastTenCountries = countries.slice(-10);
-//   return lastTenCountries;
-// }
+  const lastTenCountries = countries.slice(-10);
+  return lastTenCountries;
+}
 
-// console.log(getLastTenCountries());
+console.log(getLastTenCountries());
 
 
 // Exercise 2.7
