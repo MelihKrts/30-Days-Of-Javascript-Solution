7daf80bce45ab061f7e67daedf3c892e4dbb57e1
MelihKrts <melih.karatas1281@gmail.com>
2023-07-16 14:12:14 +0300

    code:[edited script.js]

--- Day_9_Solution/Exercise_2/script.js
+++ Day_9_Solution/Exercise_2/script.js
@@ -3,7 +3,7 @@
 // Day 9 Exercise Level 2 Solution
 
 
-const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+
 const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const products = [
