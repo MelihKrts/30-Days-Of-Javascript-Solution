371457a27c3ba005131441b887f64b2209dc10c4
MelihKrts <melih.karatas1281@gmail.com>
2023-07-20 14:54:52 +0300

    save changes

--- Day_11_Solution/Exercise_3/script.js
+++ Day_11_Solution/Exercise_3/script.js
@@ -13,8 +13,8 @@ for (const [countryKey, countryValue] of Object.entries(countries)) {
 
 // Exercise 3.2
 
-const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
-const [name, skills, [, , jsScore, reactScore]] = student;
+// const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+// const [name, skills, [, , jsScore, reactScore]] = student;
 console.log(name, skills, jsScore, reactScore);
 
 
