88f7c12663a314519051305e385221e43248c16a
MelihKrts <melih.karatas1281@gmail.com>
2023-07-22 09:42:19 +0300

    save changes

--- Day_09_Solution/Exercise_2/script.js
+++ Day_09_Solution/Exercise_2/script.js
@@ -17,8 +17,8 @@ const products = [
 
 // Exercise 2.1
 
-const totalPrice = products.map(product => product.price).filter(price => price > 0).reduce((accumulator, currentPrice) => accumulator + currentPrice, 0);
-console.log(totalPrice);
+// const totalPrice = products.map(product => product.price).filter(price => price > 0).reduce((accumulator, currentPrice) => accumulator + currentPrice, 0);
+// console.log(totalPrice);
 
 // Exercise 2.2
 
