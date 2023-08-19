79e45a1e29e876620837c3980f68b400467ee7b9
MelihKrts <melih.karatas1281@gmail.com>
2023-08-19 11:35:57 +0300

    feat:[Added day 27 solution]

---
+++ Day_27_Solution/data/challangesInfo.js
@@ -0,0 +1,35 @@
+const userData = {
+    keywords:{
+      HTML5: 'https://img.shields.io/badge/HTML5%20-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white',
+      CSS3: 'https://img.shields.io/badge/CSS%20-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white',
+      JS: 'https://img.shields.io/badge/JavaScript%20-%23F7DF1E.svg?style=for-the-badge&logo=javascript&logoColor=black',
+      GIT: 'https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white',
+      GITHUB: 'https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white',
+    },
+    author: {
+      firstName: 'Melih',
+      lastName: 'Karatas',
+      titles: [
+        ['💻', 'Programmer'],
+        ['🌐', 'JR Frontend Dev'],
+      ],
+      bio:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quo animi, delectus, harum eum earum pariatur odit enim quidem cupiditate eius est quod assumenda ab excepturi natus cumque cum labore? Nam itaque voluptatibus nihil error, doloremque inventore cum sunt eos fugit ipsa temporibus impedit dolorem voluptate? Dolore, iste similique? Labore asperiores officiis voluptas unde ab vel saepe magni vero nulla?'
+    },
+    projects: [
+      {
+        name: '',
+        githubUrl: ''
+      },
+      {
+        name: '',
+        githubUrl: ''
+      },
+      {
+        name: '',
+        githubUrl: ''
+      },
+    ]
+  }
+
+  export default userData
